a09d779d2eb1b7a48d172373e2b783a710badef3
Fernando Kuster <fernandoarielkuster@gmail.com>
2025-03-23 14:23:50 -0300

    Change directory

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ const refreshButton = document.getElementById('refresh');
 document.addEventListener('DOMContentLoaded', fetchQuote);
 
 function fetchQuote() {
-    fetch('
-https://spreadsheets.google.com/feeds/cells/2PACX-1vQ57pOctCqNWiVQvOEHhgP0OFseB2KqKCfzKIP7HHzepCcFOUuhIYjCNAkmeFad4M1GSixFBVVMBP-V/1/public/full?alt=json')
+    fetch('https://spreadsheets.google.com/feeds/cells/YOUR_SPREADSHEET_ID/1/public/full?alt=json')
         .then(response => response.json())
         .then(data => {
             const entries = data.feed.entry;
             const quotes = entries.map(entry => entry.content.$t);
             const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-            document.getElementById('quote').innerText = randomQuote;
+            quoteContainer.innerText = randomQuote;
         })
         .catch(error => console.error('Error fetching the quotes:', error));
 }
