9bc8578a1553d5f0305cb8796c4a81b43346a61e
Fernando Kuster <fernandoarielkuster@gmail.com>
2025-03-23 14:49:07 -0300

    Adds keep in memory

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const quoteContainer = document.getElementById('quote');
 const refreshButton = document.getElementById('refresh');
+let quotes = [];
 
-document.addEventListener('DOMContentLoaded', fetchQuote);
+document.addEventListener('DOMContentLoaded', fetchQuotes);
 
-function fetchQuote() {
+function fetchQuotes() {
     fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ57pOctCqNWiVQvOEHhgP0OFseB2KqKCfzKIP7HHzepCcFOUuhIYjCNAkmeFad4M1GSixFBVVMBP-V/pub?output=csv')
         .then(response => response.text())
         .then(data => {
             console.log('Fetched data:', data); // Debugging line
-            const quotes = data.split('\n').map(line => line.trim()).filter(line => line);
+            quotes = data.split('\n').map(line => line.trim()).filter(line => line);
             console.log('Quotes:', quotes); // Debugging line
-            const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-            console.log('Random Quote:', randomQuote); // Debugging line
-            quoteContainer.innerText = randomQuote;
+            displayRandomQuote();
         })
         .catch(error => console.error('Error fetching the quotes:', error));
 }
 
-refreshButton.addEventListener('click', fetchQuote);
+function displayRandomQuote() {
+    if (quotes.length > 0) {
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        console.log('Random Quote:', randomQuote); // Debugging line
+        quoteContainer.innerText = randomQuote;
+    } else {
+        quoteContainer.innerText = 'No quotes available';
+    }
+}
+
+refreshButton.addEventListener('click', displayRandomQuote);
 
 // Initial quote display
-fetchQuote();
+fetchQuotes();
